30739b99341fb1fb8b5dba4dd7de550595fb2af1
Marc Dingena <marc.dingena@gmail.com>
2021-01-14 16:57:59 +1100

    Compress all Express responses

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const compression = require("compression");
 const morgan = require("morgan");
 const session = require("express-session");
 const { createRequestHandler } = require("@remix-run/express");
 
 let app = express();
+
+// Responses should be served with compression to minimize total network bytes.
+// However, where this compression happens can vary wildly depending on your stack
+// and infrastructure. Here we are compressing all our Express responses for the
+// purpose of this starter repository, but feel free to (re)move it or change it.
+app.use(compression());
 
 app.use(express.static("public"));
 
